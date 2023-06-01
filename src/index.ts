8240d6c750d7278ddbf40e025f93abd844dd1dbe
Isaac-DeFrain <isaacdefrain@gmail.com>
2023-06-01 22:06:17 +0000

    Add index

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
-import { Add } from './Add.js';
+import { LotsOfActions1, LotsOfActions2, HugeActions } from './Actions.js';
+import { BogusProvablePure } from './BogusProvablePure.js';
+import { Circular } from './Circular.js';
+import { Hidden } from './HiddenFields.js';
+import { StateVariables } from './StateVariables.js';
+import { Transfer } from './Transfer.js';
 
-export { Add };
+export {
+  LotsOfActions1,
+  LotsOfActions2,
+  HugeActions, // action + reducer limits
+  BogusProvablePure, // fake proofs
+  Circular, // circular + recursive calls
+  Hidden, // values stored in non-method fields
+  StateVariables, // get + set state variables in loops
+  Transfer, // valid + invalid token transfers
+};
